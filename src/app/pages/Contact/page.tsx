f53d55e6e3e191d9e37584d1303cd7d142acae09
cotychunsik <toboldygo@naver.com>
2024-09-21 13:47:02 +0900

    update

--- src/app/pages/Contact/page.tsx
+++ src/app/pages/Contact/page.tsx
@@ -8,14 +8,15 @@ import { ClockIcon } from "@heroicons/react/24/outline";
 // src/app/pages/contact
 const Contact = () => {
   return (
-    <section className="bg-gray-100 py-12">
+    <section className="bg-gray-100 py-12 min-w-full">
       <div className="flex flex-row justify-content items-center container mx-auto">
-        <div className="flex min-w-96">
-          <Image className="rounded-3xl shadow-md"
+        {/* <div className="flex min-w-96">
+          <Image className="rounded-3xl shadow-md flex-1 "
           alt="contact" src="https://images.unsplash.com/photo-1551298370-9d3d53740c72?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
           width={635} height={953}></Image>
-        </div>
+        </div> */}
       <div className=" flex-1 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-left">
+        <div className="bg-contactBg bg-cover bg-center w-full h-44 sm:h-96 rounded-xl"></div>
         <h1 className="text-4xl font-extrabold text-gray-900">Get in Touch</h1>
         <p className="mt-4 text-lg leading-6 text-gray-500">Woodcraft Interiors에서 당신의 공간을 완벽하게 채울 수 있는 맞춤형 가구를 만나보세요. 지금 상담을 예약하고 꿈꾸던 공간을 실현하세요.</p>
         <div className="mt-8 tracking-wider leading-loose">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
         foreground: "var(--foreground)",
       },
       backgroundImage:{
-        "contactBg" : "url('https://images.unsplash.com/photo-1551298370-9d3d53740c72?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
+        "contactBg" : "url('https://images.unsplash.com/photo-1542744173-8e7e53415bb0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
       , "feature01" :"url('https://images.unsplash.com/photo-1712171984530-e25a4aaa46dd?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       
       },
