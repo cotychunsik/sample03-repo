0db7c7ec7d0b65c45698c8cc8205029f79f0c56b
cotychunsik <toboldygo@naver.com>
2024-09-21 21:01:31 +0900

    edit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <section className="relative bg-gray-800 flex-grow">
       <div className="container px-6 py-12 mx-auto">
         <div className="flex flex-col items-center justify-between text-sm md:flex-row">
-          <p className="mb-6 md:mb-0 text-center text-base text-gray-400">&copy; 2024 Your Company, Inc. All rights reserved.</p>
+          <p className="mb-6 md:mb-0 text-center text-base text-gray-400">&copy; 2024 WoodenCraft Interior, Inc. All rights reserved.</p>
           <ul className="flex gap-4 text-gray-400">
             <li>
               <Link href="../pages/Contact">Contact</Link>
--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="top-0 z-50 flex-shrink-0 py-4 bg-white md:sticky">
           <div className='container flex flex-col items-start justify-between px-6 mx-auto md:flex-row md:items-center'>
-            <Link href="/" className="text-lg font-bold">Logo</Link>
+            <Link href="/" className="text-lg font-bold">WD Interior.Inc</Link>
             <ul className="grid grid-flow-col gap-4 mx-auto mt-6 md:mt-0 auto-cols-auto md:auto-rows-auto md:gap-8 lg:gap-12">
               <li className="py-4 text-sm md:text-base font-normal text-gray-500 hover:text-gray-700">
                 <Link href="/" >Home</Link>
